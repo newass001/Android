026be793c46a3e96dfe6fe372fbf5ca831df5648
newass001 <47554742+newass001@users.noreply.github.com>
2020-04-15 23:58:01 +0800

    实验三

--- UiComponent/alertdialog/src/main/java/com/example/alertdialog/MainActivity.java
+++ UiComponent/alertdialog/src/main/java/com/example/alertdialog/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.alertdialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.btn);
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialog();
+
+            }
+        });
+    }
+
+    public void createDialog() {
+        TableLayout login = (TableLayout) getLayoutInflater().inflate(R.layout.activity_main, null);
+        new AlertDialog.Builder(this)
+                .setView(login)
+                .setPositiveButton("sign in", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .create()
+                .show();
     }
 }
